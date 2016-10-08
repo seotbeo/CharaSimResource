005add4a38655ff8ab2348e20484dc250435f61b
KENNYSOFT <525hm@naver.com>
2016-10-08 13:37:06 +0900

    [KMS] Item.TimeLimit

--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -1593,6 +1593,16 @@ namespace CharaSimResource {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        public static System.Drawing.Bitmap Item_timeLimit_0 {
+            get {
+                object obj = ResourceManager.GetObject("Item_timeLimit_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         public static System.Drawing.Bitmap Stat_detail_abilityTitle_normal_0 {
             get {
                 object obj = ResourceManager.GetObject("Stat_detail_abilityTitle_normal_0", resourceCulture);
