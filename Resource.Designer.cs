c7b48c854e203ca309c12948acf0e18ad3336dc2
KENNYSOFT <525hm@naver.com>
2017-07-12 21:14:09 +0900

    [KMS] Item.Protect

--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -1823,6 +1823,16 @@ namespace CharaSimResource {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        public static System.Drawing.Bitmap ItemProtector_Icon_0 {
+            get {
+                object obj = ResourceManager.GetObject("ItemProtector_Icon_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         public static System.Drawing.Bitmap Stat_detail_abilityTitle_normal_0 {
             get {
                 object obj = ResourceManager.GetObject("Stat_detail_abilityTitle_normal_0", resourceCulture);
