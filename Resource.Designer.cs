a9312b125c681509ac6cb00f3449e658df711963
KENNYSOFT <525hm@naver.com>
2017-09-24 18:16:55 +0900

    [KMS] Item

--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace CharaSimResource {
     // 클래스에서 자동으로 생성되었습니다.
     // 멤버를 추가하거나 제거하려면 .ResX 파일을 편집한 다음 /str 옵션을 사용하여 ResGen을
     // 다시 실행하거나 VS 프로젝트를 다시 빌드하십시오.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -186,6 +186,36 @@ namespace CharaSimResource {
         public static System.Drawing.Bitmap CashItem_label_3 {
             get {
                 object obj = ResourceManager.GetObject("CashItem_label_3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        public static System.Drawing.Bitmap CashItem_label_4 {
+            get {
+                object obj = ResourceManager.GetObject("CashItem_label_4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        public static System.Drawing.Bitmap CashItem_label_5 {
+            get {
+                object obj = ResourceManager.GetObject("CashItem_label_5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        public static System.Drawing.Bitmap CashItem_label_6 {
+            get {
+                object obj = ResourceManager.GetObject("CashItem_label_6", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
