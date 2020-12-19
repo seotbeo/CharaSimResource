6eae2efdc09efdd019e90098ce5d07d297122ad0
KENNYSOFT <hyeonmin.park@kennysoft.kr>
2020-12-19 18:14:55 +0900

    [KMS] Add CashItem_label.10

--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -253,6 +253,18 @@ namespace CharaSimResource {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        public static System.Drawing.Bitmap CashShop_img_CashItem_label_10
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("CashShop_img_CashItem_label_10", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         public static System.Drawing.Bitmap CSDiscount_0 {
             get {
                 object obj = ResourceManager.GetObject("CSDiscount_0", resourceCulture);
